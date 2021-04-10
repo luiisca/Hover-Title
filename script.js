0b68d7ad5ddddf1017f7e9f261ab6c7d91b1c579
LuisCadillo <luiscaf3r@gmail.com>
2021-04-10 13:08:48 -0500

    hover done

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict'
+const header = document.body.querySelector('h1');
+
+// const spanCreator = header.createElement
+const hover = function() {
+    //replace h1 by newH1 
+    const headerArr = header.textContent.split('');
+    const newHeader = document.createElement('h1');
+    header.replaceWith(newHeader);
+
+    //Adding span with each letter into newH1
+    for(const eachLetter of headerArr) {
+        const newSpan = document.createElement('span');
+        newSpan.textContent = eachLetter;
+        newHeader.append(newSpan);
+        //adding class flip into each span
+        newSpan.textContent !== ' ' && newSpan.classList.add('flip');
+    }
+} 
+header.addEventListener('mouseenter', hover);
